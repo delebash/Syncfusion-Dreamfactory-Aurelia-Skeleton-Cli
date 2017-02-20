aadde371dcdaf5043f87d58fe49f5e35e0a789f2
Daniel Elebash <danelebash@hotmail.com>
2017-02-20 09:49:57 -0500

    vendor

--- aurelia_project/tasks/build.js
+++ aurelia_project/tasks/build.js
@@ -18,6 +18,7 @@ export default gulp.series(
     processSASS,
     processCSS
   ),
+  clean,
   writeBundles,
   dist,
   copyResorces
--- src/startup/_startup.js
+++ src/startup/_startup.js
@@ -9,8 +9,6 @@ import {LogManager} from 'aurelia-framework';
 import {CustomLogAppender} from '../services/logger';
 import {UndandledErrorHandler} from "../services/errorhandler"
 
-import ej from 'ej';
-
 LogManager.addAppender(new CustomLogAppender());
 
 //#endregion
