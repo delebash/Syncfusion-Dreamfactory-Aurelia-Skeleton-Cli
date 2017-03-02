ce390f889a4ab6e0e45a941eee36e06221837754
Daniel Elebash <danelebash@hotmail.com>
2017-03-02 15:10:05 -0500

    not using

--- aurelia_project/tasks/copy-css.js
+++ aurelia_project/tasks/copy-css.js
@@ -2,6 +2,7 @@ import gulp from 'gulp';
 import cleanCSS  from 'gulp-clean-css';
 import project from '../aurelia.json';
 
+//Not currently using this task
 export default function copyCss() {
   return gulp.src(project.copyCss.sources)
     .pipe(cleanCSS())
